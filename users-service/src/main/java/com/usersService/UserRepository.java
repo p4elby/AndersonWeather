2d3680c6375faf1e92e66f2126c61904468fb80b
p4elby <pchelbyz@gmail.com>
2019-03-18 13:41:29 +0300

    end

--- users-service/src/main/java/com/usersService/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.usersService;
-
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User,Long> {
-}
